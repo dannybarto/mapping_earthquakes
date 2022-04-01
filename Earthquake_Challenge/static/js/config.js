aeedf44ff8bfd32253ca8b874a8ea17cf3ddf210
dannybarto <85522326+dannybarto@users.noreply.github.com>
2022-04-01 13:35:23 -0400

    FileCleanup

--- Earthquake_Challenge/static/js/config.js
+++ Earthquake_Challenge/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoidnJvZDIzNyIsImEiOiJja2U5YWRmeWkxc3BkMnlwZTlmMzZwNDk0In0.vrTjPu55EsZN9a-4MuRW0Q";
+const API_KEY = "pk.eyJ1IjoiZGFubnliYXJ0byIsImEiOiJjbDExOTBrNXMxdGFnM2JqbnVtbHN1d2hsIn0.eBaDNpWEc5lkJT3-PPX9VQ";
